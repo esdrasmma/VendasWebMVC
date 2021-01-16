48152a0409ffc67600b57cd6ac8b21ed27da47ad
Marcelo Esdras <esdrasmma@gmail.com>
2021-01-15 21:22:57 -0300

    Fomatando Data e valor

--- VendasWebMVC/Models/Seller.cs
+++ VendasWebMVC/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 namespace VendasWebMVC.Models
 {
@@ -7,9 +8,14 @@ namespace VendasWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
 
